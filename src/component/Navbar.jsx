e41c8cd3170f004f5d8bb7134061c5a8bc043b77
cho-sohyun <shteam1107@gmail.com>
2025-04-08 01:56:05 +0900

    부트스트랩 추가 및 상품 카드 컴포넌트 구현, 상품 목록 API 연동

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   const menuList = ["Women", "Men", "Baby", "Kids", "Home"];
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
   return (
     <div className="nav-container">
+      {/* 모바일 검색창 */}
+      {showMobileSearch && (
+        <div className="mobile-search-bar">
+          <div className="mobile-search-wrapper">
+            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+            <FontAwesomeIcon
+              icon={faXmark}
+              className="close-icon"
+              onClick={() => setShowMobileSearch(false)}
+            />
+          </div>
+        </div>
+      )}
+
       {/* 상단 바 */}
       <div className="top-bar">
         <ul className="customer-service-list">
@@ -13,17 +28,25 @@ const Navbar = () => {
           <li>고객 서비스</li>
           <li>뉴스레터</li>
         </ul>
-        <div className="login-button">
-          <FontAwesomeIcon icon={faUser} />
-          <span>로그인</span>
+
+        {/* 로그인 + 모바일 검색 아이콘 */}
+        <div className="login-and-search">
+          <div className="login-button">
+            <FontAwesomeIcon icon={faUser} />
+            <span>로그인</span>
+          </div>
+          <div
+            className="mobile-search"
+            onClick={() => setShowMobileSearch(!showMobileSearch)}
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
         </div>
       </div>
-
       {/* 로고 */}
       <div className="logo-section">
         <img width={100} src="/image/hnm-logo.jpg" alt="logo" />
       </div>
-
       {/* 메뉴 + 검색 */}
       <div className="menu-section">
         <div></div>
--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProductAll = () => {
-  return <div>ProductAll</div>;
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:4000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div className="product-all-container">
+      <Container>
+        <Row className="g-4">
+          {productList.map((menu) => (
+            <Col key={menu.id} lg={3} md={4} sm={6} xs={12}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductAll;
